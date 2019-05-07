61ab3edab5c7ed1cb260cbb2a7c2ca8d6b5d916a
xuchengzhi <xudear@live.com>
2019-05-07 15:21:24 +0800

    增加异或加密

---
+++ XorEnc.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func XorEncodeStr(msg, key string) string {
+	ml := len(msg)
+	kl := len(key)
+	// fmt.Println(string(key[ml/kl]))
+	pwd := ""
+	for i := 0; i < ml; i++ {
+		pwd += (string((key[i%kl]) ^ (msg[i])))
+	}
+
+	return pwd
+}
+
+func XorDecodeStr(msg, key string) string {
+	ml := len(msg)
+	kl := len(key)
+	pwd := ""
+	for i := 0; i < ml; i++ {
+		pwd += (string(((msg[i]) ^ key[i%kl])))
+	}
+	return pwd
+}
