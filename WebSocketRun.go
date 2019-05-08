d17819b60c7f07a0280dd6b5d17330f362acd85e
xuchengzhi <xudear@live.com>
2019-05-08 14:55:11 +0800

    增加websocket

---
+++ WebSocketRun.go
@@ -0,0 +1,59 @@
+package WebSocketRun
+
+import (
+	"fmt"
+	"golang.org/x/net/websocket"
+	"net/http"
+	"os"
+	"time"
+)
+
+//错误处理函数
+func checkErr(err error, extra string) bool {
+	if err != nil {
+		formatStr := " Err : %s\n"
+		if extra != "" {
+			formatStr = extra + formatStr
+		}
+
+		fmt.Fprintf(os.Stderr, formatStr, err.Error())
+		return true
+	}
+
+	return false
+}
+
+func svrConnHandler(conn *websocket.Conn) {
+	request := make([]byte, 128)
+	defer conn.Close()
+	nums := 0
+	for {
+		readLen, err := conn.Read(request)
+		if checkErr(err, "Read") {
+			break
+		}
+
+		//socket被关闭了
+		if readLen == 0 {
+			fmt.Println("Client connection close!")
+			break
+		} else {
+			//输出接收到的信息
+			fmt.Println(string(request[:readLen]))
+
+			time.Sleep(time.Second)
+			//发送
+			nums += 1
+			conn.Write([]byte("ojbk"))
+		}
+
+		request = make([]byte, 128)
+	}
+}
+
+func run() {
+	http.Handle("/echo", websocket.Handler(svrConnHandler))
+	err := http.ListenAndServe(":8998", nil)
+	checkErr(err, "ListenAndServe")
+	fmt.Println("Func finish.")
+}
