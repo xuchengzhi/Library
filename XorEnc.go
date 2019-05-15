312e01240214bd9cc54a0e10f527420a78709404
xuchengzhi <xudear@live.com>
2019-05-15 14:14:27 +0800

    增加哈希加密等

--- XorEnc.go
+++ XorEnc.go
@@ -1,4 +1,17 @@
-package XorEnc
+package main
+
+// package XorEnc
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"strings"
+)
 
 func XorEncodeStr(msg, key string) string {
 	ml := len(msg)
@@ -20,4 +33,78 @@ func XorDecodeStr(msg, key string) string {
 		pwd += (string(((msg[i]) ^ key[i%kl])))
 	}
 	return pwd
+}
+
+func HMAC_SHA256(src, key string) string {
+	m := hmac.New(sha256.New, []byte(key))
+	m.Write([]byte(src))
+	return hex.EncodeToString(m.Sum(nil))
+}
+
+// base编码
+func BASE64EncodeStr(src string) string {
+	return string(base64.StdEncoding.EncodeToString([]byte(src)))
+}
+
+// base解码
+func BASE64DecodeStr(src string) string {
+	a, err := base64.StdEncoding.DecodeString(src)
+	if err != nil {
+		return ""
+	}
+	return string(a)
+}
+
+var ivspec = []byte("0000000000000000")
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5Trimming(encrypt []byte) []byte {
+	padding := encrypt[len(encrypt)-1]
+	return encrypt[:len(encrypt)-int(padding)]
+}
+
+func AESEncodeStr(src, key string) string {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		fmt.Println("key error1", err)
+	}
+	if src == "" {
+		fmt.Println("plain content empty")
+	}
+	ecb := cipher.NewCBCEncrypter(block, ivspec)
+	content := []byte(src)
+	content = PKCS5Padding(content, block.BlockSize())
+	crypted := make([]byte, len(content))
+	ecb.CryptBlocks(crypted, content)
+	return hex.EncodeToString(crypted)
+}
+
+func AESDecodeStr(crypt, key string) string {
+	crypted, err := hex.DecodeString(strings.ToLower(crypt))
+	if err != nil || len(crypted) == 0 {
+		fmt.Println("plain content empty")
+	}
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		fmt.Println("key error1", err)
+	}
+	ecb := cipher.NewCBCDecrypter(block, ivspec)
+	decrypted := make([]byte, len(crypted))
+	ecb.CryptBlocks(decrypted, crypted)
+
+	return string(PKCS5Trimming(decrypted))
+}
+
+//MD5加密
+func Gmd5(msg string) string {
+	h := md5.New()
+	h.Write([]byte(msg))
+	cipherStr := h.Sum(nil)
+	fmt.Println(hex.EncodeToString(cipherStr))
+	return hex.EncodeToString(cipherStr)
 }
