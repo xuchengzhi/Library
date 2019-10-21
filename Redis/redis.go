cd173a09c71e0e0c9401da067e3ab0474ffb9721
xuchengzhi <xudear@live.com>
2019-10-21 15:58:20 +0800

    修改redis连接为connect

--- Redis/redis.go
+++ Redis/redis.go
@@ -4,7 +4,7 @@ import (
 	"github.com/xuchengzhi/redigo/redis"
 )
 
-func Redis_(ip, port, pwd string, db int) (redis.Conn, error) {
+func Connect(ip, port, pwd string, db int) (redis.Conn, error) {
 	address := fmt.Sprintf("%v:%v", ip, port)
 	RedisClient, err := redis.Dial("tcp", address, redis.DialDatabase(db), redis.DialPassword(pwd))
 	if err != nil {
